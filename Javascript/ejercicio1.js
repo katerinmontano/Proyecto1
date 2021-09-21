564504c9d200632ffeef4cb89ed0fd4edce51a37
katerinmontano <kcmontano27@gmail.com>
2021-09-20 21:50:20 -0500

    primer commit

---
+++ Javascript/ejercicio1.js
@@ -0,0 +1,18 @@
+const album3 = () =>{
+    alert("Tell Me You Love Me");
+}
+const album4 = () =>{
+    alert("Demi");
+}
+const album5 = () =>{
+    alert("Confident");
+}
+const album6 = () =>{
+    alert("Unbroken");
+}
+const album7 = () =>{
+    alert("Here we Go Again");
+}
+const album8 = () =>{
+    alert("Don´t Forget");
+}
